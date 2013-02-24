89b8ed9204c341a1f45069453877061ff09e7fa7
ForNeVeR <neverthness@gmail.com>
2013-02-24 19:02:43 +0700

    Disconnecting and simple messaging support.

--- src/main/scala/ru/org/codingteam/overland/websocket/WebSocketProcessor.scala
+++ src/main/scala/ru/org/codingteam/overland/websocket/WebSocketProcessor.scala
@@ -4,10 +4,15 @@ import akka.actor.{Actor, ActorLogging}
 import org.mashupbots.socko.events.WebSocketFrameEvent
 import org.jboss.netty.channel.Channel
 import org.jboss.netty.handler.codec.http.websocketx.TextWebSocketFrame
-import org.jivesoftware.smack.XMPPConnection
+import org.jivesoftware.smack.{Chat, MessageListener, XMPPConnection}
 import com.google.gson.Gson
+import java.lang.Throwable
+import org.jivesoftware.smack.packet.Message
 
 case class ConnectInfo(login: String, password: String)
+case class MessageInfo(to: String, text: String)
+
+case class ChatMessage(from: String, text: String)
 
 object WebSocketProcessor {
   lazy val gson = new Gson()
@@ -26,15 +31,38 @@ class WebSocketProcessor extends Actor with ActorLogging {
       channel = event.channel
       val server = "jabber.ru"
 
-      connection = new XMPPConnection(server)
-      connection.connect()
-      connection.login(info.login, info.password)
-      log.info("Login succeed")
-      send("Login succeed")
-      connected = true
+      try {
+        connection = new XMPPConnection(server)
+        connection.connect()
+        connection.login(info.login, info.password)
+        log.info("Login succeed")
+        send("Login succeed")
+        connected = true
+      } catch {
+        case exception: Throwable =>
+          log.error(exception, "XMPP connection error")
+          connected = false
+          connection.disconnect()
+          send(s"Connection error: $exception")
+      }
+    case event: WebSocketFrameEvent if connected =>
+      val info = gson.fromJson(event.readText, classOf[MessageInfo])
+      val chat = getChat(info.to)
+      chat.sendMessage(info.text)
+    case ChatMessage(from, text) =>
+      send(s"$from: $text")
   }
 
   def send(text: String) {
     channel.write(new TextWebSocketFrame(text))
+  }
+
+  def getChat(jid: String) = {
+    // TODO: remember created chats
+    connection.getChatManager.createChat(jid, new MessageListener {
+      def processMessage(chat: Chat, message: Message) {
+        self ! ChatMessage(chat.getParticipant, message.getBody)
+      }
+    })
   }
 }
