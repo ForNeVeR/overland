035eaac997a48eba8a4502ffdacd036a8b7628ed
ForNeVeR <neverthness@gmail.com>
2013-02-24 21:43:02 +0700

    Implement error handling.

--- src/main/scala/ru/org/codingteam/overland/websocket/WebSocketProcessor.scala
+++ src/main/scala/ru/org/codingteam/overland/websocket/WebSocketProcessor.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 import org.mashupbots.socko.events.WebSocketFrameEvent
 import org.jboss.netty.channel.Channel
 import org.jboss.netty.handler.codec.http.websocketx.TextWebSocketFrame
-import org.jivesoftware.smack.{Chat, MessageListener, XMPPConnection}
+import org.jivesoftware.smack.{ConnectionListener, Chat, MessageListener, XMPPConnection}
 import com.google.gson.Gson
 import java.lang.Throwable
 import org.jivesoftware.smack.packet.Message
@@ -13,6 +13,7 @@ case class ConnectInfo(login: String, password: String)
 case class MessageInfo(to: String, text: String)
 
 case class ChatMessage(from: String, text: String)
+case class CriticalError(message: String, error: Throwable)
 
 object WebSocketProcessor {
   lazy val gson = new Gson()
@@ -32,29 +33,75 @@ class WebSocketProcessor extends Actor with ActorLogging {
       val server = "jabber.ru"
 
       try {
-        connection = new XMPPConnection(server)
-        connection.connect()
-        connection.login(info.login, info.password)
-        log.info("Login succeed")
+        connect(server, info.login, info.password)
         send("Login succeed")
-        connected = true
       } catch {
-        case exception: Throwable =>
-          log.error(exception, "XMPP connection error")
-          connected = false
-          connection.disconnect()
-          send(s"Connection error: $exception")
+        case error: Throwable =>
+          self ! CriticalError("Connection error", error)
       }
+
     case event: WebSocketFrameEvent if connected =>
       val info = gson.fromJson(event.readText, classOf[MessageInfo])
       val chat = getChat(info.to)
       chat.sendMessage(info.text)
+
     case ChatMessage(from, text) =>
       send(s"$from: $text")
+
+    case CriticalError(message, error) =>
+      publishError(message, error)
+      context.stop(self)
+  }
+
+  override def postStop() {
+    log.info("Stopping processor")
+    if (connection != null) {
+      log.info("Closing connection")
+      try {
+        connection.disconnect()
+      } catch {
+        case error: Throwable =>
+          log.error(error, "Connection closing failed")
+      }
+    }
+  }
+
+  def connectionListener = new ConnectionListener {
+    def reconnectionFailed(e: Exception) {
+      self ! CriticalError("Reconnection failed", e)
+    }
+
+    def reconnectionSuccessful() {}
+
+    def connectionClosedOnError(e: Exception) {
+      self ! CriticalError("Connection closed on error", e)
+    }
+
+    def connectionClosed() {
+      self ! CriticalError("Connection closed", null)
+    }
+
+    def reconnectingIn(seconds: Int) {}
+  }
+
+  def connect(server: String, login: String, password: String) {
+    connection = new XMPPConnection(server)
+    connection.connect()
+    connection.addConnectionListener(connectionListener)
+    connection.login(login, password)
+    connected = true
   }
 
   def send(text: String) {
-    channel.write(new TextWebSocketFrame(text))
+    log.info(s"Sending: $text")
+    if (channel != null && channel.isWritable) {
+      channel.write(new TextWebSocketFrame(text))
+    }
+  }
+
+  def publishError(message: String, error: Throwable) {
+    log.error(error, message)
+    send(s"$message: $error")
   }
 
   def getChat(jid: String) = {
