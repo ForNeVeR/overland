bb1c82615660e5b66150762887ad3c61bd086b55
Friedrich von Never <friedrich@fornever.me>
2015-09-04 22:53:02 +0600

    Clean the project up, start new with stanza.io.

--- src/main/scala/ru/org/codingteam/overland/websocket/Messages.scala
+++
@@ -1,7 +0,0 @@
-package ru.org.codingteam.overland.websocket
-
-case class ConnectInfo(server: String, login: String, password: String)
-case class MessageInfo(to: String, text: String)
-
-case class ChatMessage(from: String, text: String)
-case class CriticalError(message: String, error: Throwable)
